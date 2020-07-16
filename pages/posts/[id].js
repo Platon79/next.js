7faf33896371d2b168caea1a2e3c4306950d7009
andrey_platonov <xottabbich@mail.ru>
2020-07-16 21:47:59 +0300

    Make posts pages with dynamic content use jsonplaceholder

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,53 @@
 import {useRouter} from 'next/router'
+import { useState, useEffect } from 'react'
 import MainLayout from '../../components/MainLayout'
+import Link from 'next/link'
 
-export default function Post () {
-  const router = useRouter();
+export default function Post ({ post: serverPost }) {
+  const router = useRouter()
+  const [post, setPost] = useState(serverPost)
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${router.query.id}`)
+      const json = await response.json()
+      setPost(json)
+    }
+
+    if (!serverPost) {
+      load()
+    }
+  }, []);
+
+  if (!post) {
+    return (
+      <MainLayout title="Post">
+        <p>Loading...</p>
+      </MainLayout>
+    )
+  }
 
   return (
     <MainLayout>
-      <h1>Post {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <hr />
+      <Link href={'/posts'}>
+        <a>Back to posts list</a>
+      </Link>
     </MainLayout>
   )
+}
+
+Post.getInitialProps = async ({req}) => {
+  if (!req) {
+    return { post: null }
+  }
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${req.query.id}`)
+  const post = await response.json()
+
+  return {
+    post
+  }
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,55 @@
+import {useState, useEffect} from 'react'
+import Link from 'next/link'
 import MainLayout from '../../components/MainLayout'
 
-export default function Posts () {
+export default function Posts ({ posts: serverPosts }) {
+  const [posts, setPosts] = useState(serverPosts)
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const json = await response.json()
+      setPosts(json)
+    }
+
+    if (!serverPosts) {
+      load()
+    }
+  }, [])
+
+  if (!posts) {
+    return (
+      <MainLayout title="Posts page">
+        <p>Loading...</p>
+      </MainLayout>
+    )
+  }
+
   return (
     <MainLayout title="Posts page">
       <h1>Posts page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li>
+            <Link href={`/posts/[id]`} as={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   )
+}
+
+Posts.getInitialProps = async ({req}) => {
+  if (!req) {
+    return {posts: null}
+  }
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await response.json()
+
+  return {
+    posts
+  }
 }
